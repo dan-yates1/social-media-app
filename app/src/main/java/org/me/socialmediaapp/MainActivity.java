be31822d709e66322ddc956b454576d9d04432ff
Dan Yates <dsyates@live.co.uk>
2022-01-26 20:24:05 +0000

    created additional fragments and implemented functionality for bottom nav bar

--- app/src/main/java/org/me/socialmediaapp/MainActivity.java
+++ app/src/main/java/org/me/socialmediaapp/MainActivity.java
@@ -1,8 +1,13 @@
 package org.me.socialmediaapp;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,36 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        BottomNavigationView bottomNav = findViewById(R.id.bottomNav);
+        bottomNav.setOnNavigationItemSelectedListener(navListener);
     }
+
+    private BottomNavigationView.OnNavigationItemSelectedListener navListener =
+            new BottomNavigationView.OnNavigationItemSelectedListener() {
+                @Override
+                public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                    Fragment selectedFragment = null;
+
+                    switch (item.getItemId()) {
+                        case R.id.nav_home:
+                            selectedFragment = new HomeFragment();
+                            break;
+                        case R.id.nav_messages:
+                            selectedFragment = new MessagesFragment();
+                            break;
+                        case R.id.nav_upload:
+                            selectedFragment = new UploadFragment();
+                            break;
+                        case R.id.nav_settings:
+                            selectedFragment = new SettingsFragment();
+                            break;
+                    }
+
+                    getSupportFragmentManager().beginTransaction().replace(R.id.fragmentLayout,
+                            selectedFragment).commit();
+
+                    return true;
+                }
+            };
 }
