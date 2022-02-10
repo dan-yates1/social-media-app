251423e3cdeb8b20f46cbabc20c7f2856b3576c3
Sam Smith <dsyates@live.co.uk>
2022-02-10 10:12:21 +0000

    added methods and attributes to post model

--- app/src/main/java/org/me/socialmediaapp/Post.java
+++ app/src/main/java/org/me/socialmediaapp/Post.java
@@ -4,20 +4,66 @@ import android.location.Location;
 import android.media.Image;
 import android.net.Uri;
 
+import java.util.ArrayList;
+
 public class Post {
 
     private String mTitle, mDesc;
     private Location mLocation;
     private Uri mImage;
+    private User mAuthor;
+    private ArrayList<String> mComments;
+    private int mLikes;
+    private int mCommentCount;
 
     public Post() {
     }
 
-    public Post(String mTitle, String mDesc, Location mLocation, Uri mImage) {
+    public Post(String mTitle, String mDesc, Location mLocation, Uri mImage, User author) {
         this.mTitle = mTitle;
         this.mDesc = mDesc;
         this.mLocation = mLocation;
         this.mImage = mImage;
+        this.mAuthor = author;
+        this.mComments = new ArrayList<>();
+        this.mLikes = 0;
+        this.mCommentCount = 0;
+    }
+
+    public void addComment(String comment) {
+        mComments.add(comment);
+    }
+
+    public ArrayList<String> getComments() {
+        return mComments;
+    }
+
+    public void setComments(ArrayList<String> mComments) {
+        this.mComments = mComments;
+    }
+
+    public User getAuthor() {
+        return mAuthor;
+    }
+
+    public void setAuthor(User author) {
+        this.mAuthor = author;
+    }
+
+    public int getLikes() {
+        return mLikes;
+    }
+
+    public void setLikes(int mLikes) {
+        this.mLikes = mLikes;
+    }
+
+    public int getCommentCount() {
+        return mCommentCount;
+    }
+
+    public void setComments(int commentCount) {
+        this.mCommentCount = commentCount;
     }
 
     public String getTitle() {
